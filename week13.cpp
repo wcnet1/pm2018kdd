a8788eb0238faaf5405380c7c3663ee95821fdf6
wcnet1 <45380748+wcnet1@users.noreply.github.com>
2018-11-27 15:38:15 +0900

    Add week13.cpp

---
+++ week13.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void main() {
+	/*
+	TODO: replace "XXXX-XXXXX" to your student number
+	*/
+	cout << "My student number is XXXX-XXXXX" << endl;
+}
