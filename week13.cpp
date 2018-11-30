91539f698a31a02ed6cc4f83c8f5ad3cee819482
Name <your@email.com>
2018-11-30 16:26:25 +0900

    2012-12112 박선준

--- week13.cpp
+++ week13.cpp
@@ -7,5 +7,5 @@ void main() {
 	/*
 	TODO: replace "XXXX-XXXXX" to your student number
 	*/
-	cout << "My student number is XXXX-XXXXX" << endl;
+	cout << "My student number is 2012-12112" << endl;
 }
